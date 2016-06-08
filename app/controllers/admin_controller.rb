90f4b9c3bcde34a078091e6da93886887ace94a0
Darshan <patel_darshan31@yahoo.com>
2016-06-08 22:04:27 +0000

    depot

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,11 +6,8 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class LineItem < ActiveRecord::Base
-  belongs_to :order
-  belongs_to :product
-  belongs_to :cart
-  def total_price
-    product.price * quantity
+class AdminController < ApplicationController
+  def index
+    @total_orders = Order.count
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,16 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class LineItem < ActiveRecord::Base
-  belongs_to :order
-  belongs_to :product
-  belongs_to :cart
-  def total_price
-    product.price * quantity
-  end
+class User < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end     
 end
